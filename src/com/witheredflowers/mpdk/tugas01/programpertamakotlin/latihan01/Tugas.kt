dbd887f0077053cad9f8dc0c301f28eed775c387
unknown <withered-flowers@users.noreply.github.com>
2019-08-06 22:14:31 +0700

    Memulai Pemrograman Kotlin done

---
+++ src/com/witheredflowers/mpdk/tugas01/programpertamakotlin/latihan01/Tugas.kt
@@ -0,0 +1,5 @@
+package com.witheredflowers.mpdk.tugas01.programpertamakotlin.latihan01
+
+fun main() {
+    println("Kotlin is Awesome !")
+}
---
+++ src/com/witheredflowers/mpdk/tugas01/programpertamakotlin/latihan02/Tugas.kt
@@ -0,0 +1,9 @@
+package com.witheredflowers.mpdk.tugas01.programpertamakotlin.latihan02
+
+fun main() {
+    println(
+        """"Kotlin,
+            |is Awesome!
+        """.trimMargin()
+    )
+}
